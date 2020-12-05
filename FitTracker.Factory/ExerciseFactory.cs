9fafac08e0ad99b276da7353b3d244ea78331f41
SemWenders <66679237+SemWenders@users.noreply.github.com>
2020-12-05 22:51:18 +0100

    added factories

---
+++ FitTracker.Factory/ExerciseFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FitTracker.Persistence;
+using FitTracker.Interface.Interfaces;
+
+namespace FitTracker.Factory
+{
+    static class ExerciseFactory
+    {
+        static IExerciseDAL GetExerciseDAL()
+        {
+            return new ExerciseDAL;
+        }
+    }
+}
---
+++ FitTracker.Factory/TrainingFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FitTracker.Interface.Interfaces;
+using FitTracker.Persistence;
+
+namespace FitTracker.Factory
+{
+    static class TrainingFactory
+    {
+        static ITrainingDAL GetTrainingDAL()
+        {
+            return new TrainingDAL();
+        }
+    }
+}
