048b20b9b6b71d9578a4cea86c2c9febbd08c26b
SemWenders <66679237+SemWenders@users.noreply.github.com>
2020-12-06 11:31:57 +0100

    added constructors for dtos

--- Fitness_Applicatie_Interface/DTOs/TrainingDTO.cs
+++ Fitness_Applicatie_Interface/DTOs/TrainingDTO.cs
@@ -6,10 +6,20 @@ namespace FitTracker.Interface.DTOs
 {
     public class TrainingDTO
     {
-        public string trainingID { get; private set; }
+        public string TrainingID { get; private set; }
         public string UserID { get; private set; }
         public DateTime Date { get; private set; }
-        public List<ExerciseDTO> ExerciseNames { get; private set; }
+        public List<ExerciseDTO> Exercises { get; private set; }
         public List<SetDTO> Sets { get; private set; } //list of weight of each set
+
+        //constructor
+        public TrainingDTO(string trainingID, string userID, DateTime date, List<ExerciseDTO> exercises, List<SetDTO> sets)
+        {
+            this.TrainingID = trainingID;
+            this.UserID = userID;
+            this.Date = date;
+            this.Exercises = exercises;
+            this.Sets = sets;
+        }
     }
 }
