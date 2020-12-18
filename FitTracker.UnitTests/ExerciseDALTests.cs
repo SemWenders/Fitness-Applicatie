1a3c990784a1c93684451be60124594006c49455
SemWenders <66679237+SemWenders@users.noreply.github.com>
2020-12-18 16:04:49 +0100

    unit tests

---
+++ FitTracker.UnitTests/ExerciseDALTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FitTracker.Interface.DTOs;
+using FitTracker.Logic;
+
+namespace FitTracker.UnitTests
+{
+    [TestClass]
+    public class ExerciseDALTests
+    {
+        [TestMethod]
+        public void AddExercise_Correctly()
+        {
+            User user = new User
+            {
+                UserID = Guid.NewGuid()
+            };
+            Exercise exercise = new Exercise("Deadlift", false, user.UserID);
+            user.AddExercise(exercise);
+        }
+    }
+}
---
+++ FitTracker.UnitTests/TrainingDALTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FitTracker.Interface.DTOs;
+using FitTracker.Logic;
+
+namespace FitTracker.UnitTests
+{
+    [TestClass]
+    class TrainingDALTests
+    {
+        [TestMethod]
+        public void AddTraining_Correctly()
+        {
+            User user = new User
+            {
+                UserID = Guid.NewGuid()
+            };
+
+            Training training = new Training
+            {
+                trainingID = Guid.NewGuid(),
+                Date = DateTime.Now,
+                UserID = user.UserID,
+                ExercisesNames = new List<Exercise>
+                {
+                    new Exercise("Deadlift", false, user.UserID),
+                    new Exercise("Benchpress", false, user.UserID),
+                    new Exercise("Push Up", true, user.UserID),
+                    new Exercise("Chin Up", true, user.UserID)
+                },
+
+                Sets = new List<Set>
+                {
+                    new Set {Weights = new List<double> {80, 85, 90}},
+                    new Set {Weights = new List<double> {50, 52.5, 55}},
+                    new Set {Weights = new List<double> {7, 7, 7}},
+                    new Set {Weights = new List<double> {15, 15, 15}}
+                }
+            };
+            user.AddTraining(training);
+        }
+    }
+}
